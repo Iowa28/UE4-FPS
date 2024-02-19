2bdceb0452e6edae3bbca9d5f0f10b35f48e0a8e
naminov <naminov@tko-inform.ru>
2024-02-19 13:13:05 +0300

    remove unnecessary files

--- UdemyProject3/Intermediate/Build/Win64/UdemyProject3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30153
-#include "C:/Users/naminov/Documents/Unreal Projects/UE4-Tank-Battle/UdemyProject3/Intermediate/Build/Win64/UdemyProject3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
