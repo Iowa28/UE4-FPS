6e2bece424c881a008a2a99221bced38053ebf2b
naminov <naminov@tko-inform.ru>
2024-03-05 09:28:47 +0300

    delete player folder and rename NPC

--- UdemyProject3/Source/UdemyProject3/UdemyProject3GameMode.cpp
+++ UdemyProject3/Source/UdemyProject3/UdemyProject3GameMode.cpp
@@ -2,14 +2,13 @@
 
 #include "UdemyProject3GameMode.h"
 #include "UdemyProject3HUD.h"
-#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AUdemyProject3GameMode::AUdemyProject3GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Behaviour/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- UdemyProject3/Source/UdemyProject3/UdemyProject3HUD.cpp
+++ UdemyProject3/Source/UdemyProject3/UdemyProject3HUD.cpp
@@ -10,7 +10,7 @@
 AUdemyProject3HUD::AUdemyProject3HUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Character/Models/FirstPerson/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
